1b4df04acfae4f4524a0b1da02824131d866bc26
Troy <troyakins99@yahoo.com>
2021-07-20 18:29:21 -0500

    commit tree c3a9f476704670b4a8791cf809a28118f0e6073f

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,13 @@
 import React from "react";
+import NavBar from "./NavBar";
+import Home from "./Home";
+import About from "./About";
 
 function App() {
   return (
     <div>
       <NavBar />
-      <Home />
+      <Home username="Troy" city="Houston" />
       <About />
     </div>
   );
